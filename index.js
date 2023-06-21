e92e0070f9415ce43cc94cb893742a254a785660
zaynmuhammad <zaynmuhammad4@hotmail.com>
2023-06-21 15:55:23 -0400

    Update cors endpoint

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server, {
     cors: {
-        origin: 'https://chat-app-zayn.netlify.app',
+        origin: 'https://main--chat-app-zayn.netlify.app',
         methods: ["GET", "POST"],
     }
 
